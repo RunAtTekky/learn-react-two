0fdb2e3d22929c6197d17f5183376e433c7d6a8d
Varun <varunrawat343@gmail.com>
2024-06-12 18:16:50 +0530

    Remove Button from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
